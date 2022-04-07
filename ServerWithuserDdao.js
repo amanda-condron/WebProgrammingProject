7d7996a4b89585bfc7376bfe5e23c8c04de6253c
amandacondron <acondron@loyola.edu>
2022-04-07 10:14:20 -0400

    creating DAO, stuck on update user, tags may be messed up

---
+++ ServerWithuserDdao.js
@@ -0,0 +1,77 @@
+const express = require('express'); 
+const morgan = require('morgan'); 
+const dao = require('./model/usersDogsDaoMem');
+
+let port = 4000;
+let hostname = 'localhost';
+
+const app = express(); 
+app.use(morgan('dev')); 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static('public_html'));
+
+//GET ALL DOGS
+app.get('/usersDogs',function(req,res){ 
+    res.status(200);
+    res.send(dao.readAll());
+    res.end(); 
+});
+
+app.get('/usersDogs/:id',function(req,res){
+    let id = parseInt( req.params.id );
+    let found = dao.read(id);
+
+    if(found !== null)
+    { 
+        res.status(200);
+        res.send(found);
+    }
+    else
+    {
+        res.status(404);
+        res.send({msg:'Dog not found.'}); 
+    }
+    res.end(); 
+});
+
+app.post('/usersDogs',function(req,res){
+    let newDog = {};
+    newDog.name = req.body.breed_name;
+    newDog.weight = req.body.dog_weight;
+    newDog.dog_LifeExp = req.body.dog_LifeExp;
+    newDog.description = req.body.dog_description;
+
+    if(req.body.txt_id)
+    {
+        console.log('Update Dog');
+        newDog.txt_id = req.body.txt_id;
+        console.log('dog id::::: ', newDog.txt_id);
+        dao.update(newDog);
+    }
+    else
+    {
+        dao.create(newDog);        
+    }
+    res.redirect('index.html');
+});
+
+app.get('/delusersDogs/:id',function(req,res){
+    //URL parameter always on req.params.<name>
+    let id = parseInt( req.params.id );   
+    
+    dao.del(id);
+
+    res.redirect('../index.html');
+}); //end of app.get(deluser)
+
+/*app.post('/updateusersDogs',function(req,res){
+    //Not being called, the front-end sends to the same function as the create new user
+
+}); */
+
+
+app.listen(port,hostname,function(){ // Listen to client requests in hostname:port
+    console.log(`Server Running on ${hostname}:${port}...`);
+});
--- model/usersDogsDaoMem.js
+++ model/usersDogsDaoMem.js
@@ -1,4 +1,8 @@
-exports.usersDogs = [];
+exports.usersDogs = [
+    { _id:1,name:'German Spitz', weight:'20 - 24 lbs', lifeExp:'13 - 15 years', description:'This foot-tall ball of fluff looks like a larger Pomeranian, but that is where the similarity ends. The spitz is also a working dog who was bred to hunt small prey.'},
+    { _id:2,name:'Lowchen', weight:'15 lbs', lifeExp:'13 - 15 years', description:'The Lowchen or “little lion dog” hails from France and Germany and was bred as a royal footwarmer with a distinctive haircut that leaves them fluffy at the front and naked in back. This rare breed has been around since at least the 16th century.'},
+    { _id:3,name:'Papillon', weight:'5 - 10 lbs', lifeExp:'14 - 16 years', description:'The Pap looks like a dainty lap dog with a plumed tail but is nonetheless robust and eager to play. The breed does well in agility competitions and thrives in any climate or home setting.'},
+];
 
 exports.readAll = function(){
     return exports.usersDogs;
@@ -31,8 +35,34 @@ exports.del = function(id){
     return deleteDog;
 }
 
-exports.update = function(id){
-    //homework
+/*exports.update = function(newDog){
+    let index = pos(id);
+    let existingDog = null;
+    if(index >= 0)
+    {
+        exports.usersDogs[index] = existingDog;
+        exports.usersDogs.splice(index, 0, existingDog);
+    }
+    return existingDog;
+}*/
+
+exports.update = function(newDog){
+        let index = 1;
+        let existingDog = {};
+        existingDog._id = newDog._id;
+        console.log('exisitng DOG ID IS: ', existingDog._id);
+        if(existingDog.name !== newDog.name)
+            existingDog.name = newDog.name;
+        if(existingDog.weight !== newDog.weight)
+            existingDog.weight = newDog.weight;
+        if(existingDog.lifeExp !== newDog.lifeExp)
+            existingDog.lifeExp = newDog.lifeExp;
+        if(existingDog.description !== newDog.description)
+            existingDog.description = newDog.description;
+        
+        exports.usersDogs.splice(newDog._id,1);
+        exports.usersDogs.splice(newDog._id, 0, existingDog);
+        return existingDog;
 }
 
 function pos(id){
